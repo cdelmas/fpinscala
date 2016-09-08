59d73845b84add45b6379fc3c0cc2a040316acf5
cdelmas <cyril.delmas@gmail.com>
2016-09-08 13:43:30 +0200

    Add more functions to Gen

--- src/fpinscala/propertybasedtesting/Gen.scala
+++ src/fpinscala/propertybasedtesting/Gen.scala
@@ -5,11 +5,24 @@ import fpinscala.functionalstate.{RNG, State}
 /**
   * Created by c.delmas on 22/07/2016.
   */
-case class Gen[A](sample: State[RNG, A])
+case class Gen[+A](sample: State[RNG, A]) {
+  def flatMap[B](f: A => Gen[B]): Gen[B] =
+    Gen(sample.flatMap(a => f(a).sample))
+
+  def listOfN(size: Gen[Int]): Gen[List[A]] =
+    size flatMap (n => Gen.listOfN(n, this))
+
+  def map[B](f: A => B): Gen[B] =
+    Gen(sample.map(f))
+
+  def map2[B,C](g: Gen[B])(f: (A,B) => C): Gen[C] =
+    Gen(sample.map2(g.sample)(f))
+
+}
 
 object Gen {
 
-  def choose(start:Int, stopExclusive:Int): Gen[Int] =
+  def choose(start: Int, stopExclusive: Int): Gen[Int] =
     Gen(State(RNG.map(RNG.positiveLessThan(stopExclusive - start))(_ + start)))
 
   def unit[A](a: => A): Gen[A] =
@@ -21,8 +34,12 @@ object Gen {
   def listOfN[A](n: Int, g: Gen[A]): Gen[List[A]] =
     Gen(State.sequence(List.fill(n)(g.sample)))
 
-  
+  def union[A](g1: Gen[A], g2: Gen[A]): Gen[A] =
+    boolean flatMap (b => if (b) g1 else g2)
+
+  def weighted[A](g1: (Gen[A],Double), g2: (Gen[A],Double)): Gen[A] = {
+    val threshold = g1._2.abs / (g1._2.abs + g2._2.abs)
+    Gen(State(RNG.nextDouble).flatMap(d => if (d < threshold) g1._1.sample else g2._1.sample))
+  }
 }
-
-
 
