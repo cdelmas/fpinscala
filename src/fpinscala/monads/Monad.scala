34f831537f49921d72e9cc44dcdf300591a9e088
cdelmas <cyril.delmas@gmail.com>
2017-01-12 17:27:30 +0100

    Add the Id and Reader monads

--- src/fpinscala/monads/Monad.scala
+++ src/fpinscala/monads/Monad.scala
@@ -33,6 +33,10 @@ trait Monad[F[_]] extends Functor[F] {
 
   def flatMap_[A, B](ma: F[A])(f: A => F[B]): F[B] = compose((_: Unit) => ma, f)()
 
+  def join_[A](mma: F[F[A]]): F[A] = flatMap(mma)(identity)
+
+  def flatMap_j[A, B](ma: F[A])(f: A => F[B]): F[B] = join_(map(ma)(f))
+
 }
 
 object Monads {
@@ -86,4 +90,45 @@ object Monads {
     override def flatMap[A, B](ma: State[S, A])(f: (A) => State[S, B]): State[S, B] = ma flatMap f
   }
 
+  val idMonad = new Monad[Id] {
+    def unit[A](a: => A) = Id(a)
+
+    override def flatMap[A, B](ida: Id[A])(f: A => Id[B]): Id[B] = ida flatMap f
+  }
+}
+
+case class Id[A](value: A) {
+
+  def flatMap[B](f: A => Id[B]): Id[B] = f(value)
+
+  def map[B](f: A => B): Id[B] = Id(f(value))
+}
+
+// The action of Reader's `flatMap` is to pass the `r` argument along to both the
+// outer Reader and also to the result of `f`, the inner Reader. Similar to how
+// `State` passes along a state, except that in `Reader` the "state" is read-only.
+
+// The meaning of `sequence` here is that if you have a list of functions, you can
+// turn it into a function that takes one argument and passes it to all the functions
+// in the list, returning a list of the results.
+
+// The meaning of `join` is simply to pass the same value as both arguments to a
+// binary function.
+
+// The meaning of `replicateM` is to apply the same function a number of times to
+// the same argument, returning a list of the results. Note that if this function
+// is _pure_, (which it should be), this can be exploited by only applying the
+// function once and replicating the result instead of calling the function many times.
+// This means the Reader monad can override replicateM to provide a very efficient
+// implementation.
+case class Reader[R, A](run: R => A)
+
+object Reader {
+  def readerMonad[R] = new Monad[({type l[x] = Reader[R, x]})#l] {
+    def unit[A](a: => A): Reader[R, A] = Reader(_ => a)
+
+    def flatMap[A, B](st: Reader[R, A])(f: A => Reader[R, B]): Reader[R, B] = Reader(r => f(st.run(r)).run(r))
+  }
+
+  def ask[R]: Reader[R, R] = Reader(r => r)
 }
